c049d3ea75cb70c9db2e98dff67a21a2177c5a27
Alexis20-1 <arumana@cemcol.com>
2024-08-17 22:09:36 -0600

    calculadora java

---
+++ Calculadora.java
@@ -0,0 +1,16 @@
+ /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadora;
+
+/**
+ *
+ * @author alexi
+ */
+public class Calculadora {
+       public static void main(String[] args) {
+        Pantalla pantallita = new Pantalla();
+        pantallita.setVisible(true);
+    }
+}
